1fe6be0ef9be1b35a62863e1db41f04208c1caa8
syarusinsky <siike92@hotmail.com>
2021-11-13 18:27:08 -0600

    Added look-ahead limiter class for clipping prevention

---
+++ src/Limiter.cpp
@@ -0,0 +1,58 @@
+#include "Limiter.hpp"
+
+#include "AudioConstants.hpp"
+
+#include <cmath>
+
+Limiter::Limiter (float attackTime, float releaseTime, float threshold, float makeupGain) :
+	m_AttackTime( attackTime ),
+	m_AttackCoeff( (1.0f - pow(M_E, (-2.2f * SAMPLE_PERIOD) / (m_AttackTime / 1000.0f))) * 2 ),
+	m_ReleaseTime( releaseTime ),
+	m_ReleaseCoeff( (1.0f - pow(M_E, (-2.2f * SAMPLE_PERIOD) / (m_ReleaseTime / 1000.0f))) * 2 ),
+	m_Threshold( threshold ),
+	m_MakeupGain( makeupGain ),
+	m_Peak( 0.0f ),
+	m_Coefficient( 0.0f ),
+	m_Gain( 1.0f ),
+	m_CircularBufferLength( (SAMPLE_RATE / 1000) * m_AttackTime ),
+	m_CircularBuffer( new float[m_CircularBufferLength] ),
+	m_WriteIndex( 0 ),
+	m_ReadIndex( 1 )
+{
+	for ( unsigned int sample = 0; sample < m_CircularBufferLength; sample++ )
+	{
+		m_CircularBuffer[sample] = 0.0f;
+	}
+}
+
+Limiter::~Limiter()
+{
+	delete[] m_CircularBuffer;
+}
+
+void Limiter::call (float* writeBuffer)
+{
+	for ( unsigned int sample = 0; sample < ABUFFER_SIZE; sample++ )
+	{
+		// envelope follower
+		float absoluteSampleVal = std::fabs( writeBuffer[sample] );
+
+		m_Coefficient = ( absoluteSampleVal > m_Peak ) ? m_AttackCoeff : m_ReleaseCoeff;
+
+		m_Peak = ( (1.0f - m_Coefficient) * m_Peak ) + ( m_Coefficient * absoluteSampleVal );
+
+		// find gain to apply to signal
+		float filter = std::fmin( 1.0f, m_Threshold / m_Peak );
+
+		m_Coefficient = ( filter < m_Gain ) ? m_AttackCoeff : m_ReleaseCoeff;
+
+		m_Gain = ( (1.0f - m_Coefficient) * m_Gain ) + ( m_Coefficient * filter );
+
+		// apply gain to delayed samples in circular buffer
+		m_CircularBuffer[m_WriteIndex] = writeBuffer[sample];
+		writeBuffer[sample] = m_Gain * m_CircularBuffer[m_ReadIndex] * m_MakeupGain;
+
+		m_ReadIndex = ( m_ReadIndex + 1 ) % m_CircularBufferLength;
+		m_WriteIndex = ( m_WriteIndex + 1 ) % m_CircularBufferLength;
+	}
+}
